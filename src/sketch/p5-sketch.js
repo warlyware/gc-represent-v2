1efcfe7bce6689f834812ba9fad190a7fc90a1d1
Dan Ward <daniel.ward@forrent.com>
2017-11-03 16:54:04 -0400

    Add sphere, lighting

--- src/sketch/p5-sketch.js
+++ src/sketch/p5-sketch.js
@@ -3,11 +3,15 @@ import p5 from 'p5';
 const sketch = (p5) => {
     let canvas;
     p5.setup = () => {
-        canvas = p5.createCanvas(600, 600);
+        canvas = p5.createCanvas(600, 600, p5.WEBGL);
         canvas.parent('sketch');
     }
     p5.draw = () => {
-        p5.background('#111');
+        p5.background(51);
+
+        p5.noStroke();
+        p5.sphere(200);
+        p5.directionalLight(248, 117, 157, 0.5, 0.5, 0.5);
     }
 }
 
