16391e790314224d7094edfd1edc2df4be5af3fa
warlyware <danward@gmail.com>
2017-11-04 16:30:59 -0400

    Fix globe coordinate placement maths

--- src/sketch/globe-sketch.js
+++ src/sketch/globe-sketch.js
@@ -4,8 +4,8 @@ import users from '../data/users.js';
 const sketch = (p5) => {
     let canvas;
     let earth;
-    const radius = 400;
-    const userSize = 30;
+    const radius = 200;
+    const userSize = 5;
     const heightFromSurface = -15;
 
 
@@ -21,29 +21,30 @@ const sketch = (p5) => {
     p5.draw = () => {
         p5.background(20);
         p5.texture(earth, 0, 0);
-        p5.rotateY(p5.frameCount * 0.01);
-        p5.angle -= 0.05;
+        p5.rotateY(p5.frameCount * -0.01);
 
         p5.pointLight(255, 255, 255, 0, 0, 200);
 
         p5.noStroke();
         p5.sphere(radius);
-
+        p5.rotateY(-1.5);
+        p5.rotateZ(0.1);
         for(const [userName, user] of Object.entries(users)) {
-            let longitude = 144.9631;
-            let latitude = -37.8136;
+            const longitude = p5.radians(user.longitude);
+            const latitude = p5.radians(user.latitude);
 
-            let theta = p5.radians(user.latitude) + p5.PI/2;
-            let phi = p5.radians(-user.longitude) + p5.PI;
+            let cx = radius * p5.cos(latitude) * p5.cos(longitude);
+            let cy = radius * p5.cos(latitude) * p5.sin(longitude);
+            let cz = radius * p5.sin(latitude);
 
-            let x = radius * p5.sin(theta) * p5.cos(phi);
-            let y = radius * p5.sin(theta) * p5.sin(phi);
-            let z = radius * p5.cos(theta);
+            let x = -cx;
+            let y = -cz;
+            let z = cy;
 
             p5.push();
             p5.translate(x, y, z);
             p5.fill(255, 255, 255);
-            p5.box(userSize);
+            p5.sphere(userSize);
             p5.pop();
         }
     }
