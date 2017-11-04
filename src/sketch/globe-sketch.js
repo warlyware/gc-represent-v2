e99e6eb2e3be0d29c0b5a8c3392facf6dde667a4
warlyware <danward@gmail.com>
2017-11-04 14:54:24 -0400

    Add earth texture, improve lighting

--- src/sketch/globe-sketch.js
+++ src/sketch/globe-sketch.js
@@ -1,12 +1,17 @@
 import p5 from 'p5';
+import users from '../data/users.js';
 
 const sketch = (p5) => {
-    let longitude = 144.9631;
-    let latitude = -37.8136;
     let canvas;
-    const radius = 200;
-    const userSize = 10;
+    let earth;
+    const radius = 400;
+    const userSize = 30;
     const heightFromSurface = -15;
+
+
+    p5.preload = () => {
+        earth = p5.loadImage('https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74218/world.200412.3x5400x2700.jpg');
+    }
 
     p5.setup = () => {
         canvas = p5.createCanvas(window.innerWidth, window.innerHeight, p5.WEBGL);
@@ -15,22 +20,32 @@ const sketch = (p5) => {
 
     p5.draw = () => {
         p5.background(20);
+        p5.texture(earth, 0, 0);
         p5.rotateY(p5.frameCount * 0.01);
-        p5.angle += 0.05;
+        p5.angle -= 0.05;
 
-        p5.directionalLight(255, 255, 255, 0.5, 0.8, 0.5);
+        p5.pointLight(255, 255, 255, 0, 0, 200);
+
         p5.noStroke();
         p5.sphere(radius);
 
-        let theta = p5.radians(latitude) + p5.PI * 2;
-        let phi = p5.radians(longitude) + p5.PI;
-        let x = radius * p5.sin(theta) * p5.cos(phi);
-        let y = -radius * p5.sin(theta) * p5.cos(phi);
-        let z = radius * p5.cos(theta);
-        console.log(z);
-        p5.translate(x, y, z + heightFromSurface);
-        p5.directionalLight(255, 0, 255, 0.5, 0.8, 0.5);
-        p5.sphere(userSize);
+        for(const [userName, user] of Object.entries(users)) {
+            let longitude = 144.9631;
+            let latitude = -37.8136;
+
+            let theta = p5.radians(user.latitude) + p5.PI/2;
+            let phi = p5.radians(-user.longitude) + p5.PI;
+
+            let x = radius * p5.sin(theta) * p5.cos(phi);
+            let y = radius * p5.sin(theta) * p5.sin(phi);
+            let z = radius * p5.cos(theta);
+
+            p5.push();
+            p5.translate(x, y, z);
+            p5.fill(255, 255, 255);
+            p5.box(userSize);
+            p5.pop();
+        }
     }
 }
 
